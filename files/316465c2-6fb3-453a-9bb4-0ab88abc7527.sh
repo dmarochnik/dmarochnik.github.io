a3dcaae3b4bf597bf6c71c27ca45de13320a4612
dmarochnik <55168919+dmarochnik@users.noreply.github.com>
2019-11-13 20:00:33 -0700

    upload

---
+++ files/316465c2-6fb3-453a-9bb4-0ab88abc7527.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#I, Daria Marochnik, made this script. It is original.
+#March 7, 2019
+#Version 1.0
+#
+read -p "Ho ho ho! Please enter your name: " NAME
+echo "Hello, $NAME. I'm BoBo, and I'm bored. Will you play a game with me?"
+read -p "Enter Y for yes: " Y
+echo "You're the best, $NAME. OK, I picked a number between 1 and 100."
+n1=$[($RANDOM % 100) +1]
+echo -n "Please guess what it is:  "
+while counter=$(($counter+1))
+read n2; do
+
+if   [[ $n2 -eq $n1 ]]; then
+echo "Wowzers, $NAME! Fantastic! It only took you $counter guesses to get the right number."
+exit 0;
+else
+echo
+if [[ $n2 -gt $n1 ]]; then
+echo -n "Sorry, $NAME, your guess is too high. New guess:"
+elif [[ $n2 -lt $n1 ]]; then
+echo -n "Sorry, $NAME, your guess is too low. New guess:"
+fi
+fi
+
+done
+
---
+++ files/HTMLproject/Project8/myscripts.js
@@ -0,0 +1,36 @@
+﻿
+var imgArray = new Array(
+ 'Image1Lg.jpg',
+ 'Image2Lg.jpg',
+ 'Image3Lg.jpg',
+ 'Image4Lg.jpg'
+
+ ); 
+ var titleArray = new Array(
+ 'Cat and Dog',
+ 'Girl with a hamster',
+ 'Horse',
+ 'German Shepherd'
+
+ );
+
+ var imgPath = "Images/FullSize/";
+
+ function swapImage(imgID) {
+ var theImage = document.getElementById('theImage');
+ var textDiv = document.getElementById('bottomText');
+ var newImg;
+ var textTitle;
+ newImg = imgArray[imgID];
+ theImage.src = imgPath + newImg;
+ textTitle=titleArray[imgID];
+ textDiv.innerHTML = textTitle;
+ }
+
+ function preloadImages() {
+ for(var i = 0; i <imgArray.length; i++) {
+ var tmpImg = new Image;
+ tmpImg.src = imgPath + imgArray[i];
+ }
+ 
+}
